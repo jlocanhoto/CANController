3050339c55a4566589c192fc9fe9b9a228b0c858
jlocx <jloc@cin.ufpe.br>
2018-11-11 11:31:35 -0300

    Separated constructor from setup function

--- BTL.cpp
+++ BTL.cpp
@@ -7,7 +7,12 @@ void TimeQuantum()
     digitalWrite(LED, digitalRead(LED) ^ 1);
 }
 
-BitTimingLogic::BitTimingLogic(uint32_t _TQ, int8_t _T1, int8_t _T2, int8_t _SJW)
+BitTimingLogic::BitTimingLogic()
+{
+
+}
+
+void BitTimingLogic::setup(uint32_t _TQ, int8_t _T1, int8_t _T2, int8_t _SJW)
 {
     this->limit_TSEG1 = _T1;
     this->limit_TSEG2 = _T2;

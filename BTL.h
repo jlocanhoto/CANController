cfb50231cea498ce7014f117eccbebfcdcb104d4
jlocx <jloc@cin.ufpe.br>
2018-11-11 11:28:30 -0300

    Added two public functions to be called on the main routine

--- BTL.h
+++ BTL.h
@@ -9,11 +9,13 @@ class BitTimingLogic {
         int8_t limit_TSEG2;
         int8_t sjw;
         void frequency_divider(uint32_t TQ);
+        void edge_detector(bool scaled_clock, bool bus_idle);
+        void bit_segmenter(bool &sample_point, bool &writing_point);
 
     public:
-        BitTimingLogic(uint32_t _TQ, int8_t _T1, int8_t _T2, int8_t _SJW);
-        void edge_detector(bool scaled_clock, bool bus_idle);
-        void bit_segmenter(bool &sample_point, bool &writing_point);        
+        BitTimingLogic();
+        void setup(uint32_t _TQ, int8_t _T1, int8_t _T2, int8_t _SJW);
+        void run(bool input_bit, bool write_bit, bool &sample_bit, bool &output_bit);
 };
 
 #endif

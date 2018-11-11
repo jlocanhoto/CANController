15ac1c7310213a8a867a6092f2a18de6dccd4f89
jlocx <jloc@cin.ufpe.br>
2018-11-11 11:30:54 -0300

    Added initial CAN controller logic with simulated values

--- can_controller.ino
+++ can_controller.ino
@@ -1,13 +1,42 @@
 #include "BTL.h"
 
+// sequência de bits do frame de input
+bool input[]      = {false, true, true, true, false, true};
+// sequência de bits do frame de input
+bool output[]     = {false, true, false, false, true, true};
+// posição do bit segmentado em que ocorre o respectivo bit de input
+uint8_t seg_pos[] = {  0  ,  0  ,  0  ,  0  ,   0  ,  0  };
+
+BitTimingLogic BTL;
+
 void setup()
 {
     pinMode(LED, OUTPUT);
     Serial.begin(115200);
-    BitTimingLogic BTL(TQ, T1, T2, SJW);
+    BTL.setup(TQ, T1, T2, SJW);
 }
 
 void loop()
 {
+    static uint8_t i = 0, j = 0;
+    
+    //bool input_bit = digitalRead(...);
+    bool input_bit = input[i];
+    bool output_bit;
+    bool sampled_bit;
+    //bool write_bit = digitalRead(...);
+    bool write_bit = output[i];
 
+    BTL.run(input_bit, write_bit, sampled_bit, output_bit);
+    
+    if (i < 6) {
+        Serial.print(sampled_bit, DEC);
+        if (i < 5) {
+            Serial.print(", ");
+        }
+        else {
+            Serial.println();
+        }
+        i++;
+    }
 }

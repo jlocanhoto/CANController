b7e5cd423d5ed4d9a3e86ee1d2825081699ef8d2
jlocx <jloc@cin.ufpe.br>
2018-11-11 12:58:11 -0300

    Updated method definitions to include additional I/O signals

--- BTL.h
+++ BTL.h
@@ -9,13 +9,13 @@ class BitTimingLogic {
         int8_t limit_TSEG2;
         int8_t sjw;
         void frequency_divider(uint32_t TQ);
-        void edge_detector(bool scaled_clock, bool bus_idle);
+        void edge_detector(bool input_bit, bool scaled_clock, bool bus_idle);
         void bit_segmenter(bool &sample_point, bool &writing_point);
 
     public:
         BitTimingLogic();
         void setup(uint32_t _TQ, int8_t _T1, int8_t _T2, int8_t _SJW);
-        void run(bool input_bit, bool write_bit, bool &sample_bit, bool &output_bit);
+        void run(bool input_bit, bool write_bit, bool &sample_bit, bool &output_bit, bool bus_idle, bool &sample_point, bool &writing_point);
         bool simulate(bool reach_segment, uint8_t &j);
 };
 

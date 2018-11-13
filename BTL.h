70fd4c6bebabd370bcd2e50e06797cf43ee3faf6
jlocx <jloc@cin.ufpe.br>
2018-11-12 21:09:18 -0300

    Modified methods to receive previous and current input bits

--- BTL.h
+++ BTL.h
@@ -9,8 +9,8 @@ class BitTimingLogic {
         int8_t limit_TSEG2;
         int8_t sjw;
         void frequency_divider(uint32_t);
-        void edge_detector(bool prev_input_bit, bool input_bit, bool &bus_idle);
-        void bit_segmenter(bool &sample_point, bool &writing_point);
+        void edge_detector(bool &prev_input_bit, bool input_bit, bool &bus_idle);
+        void bit_segmenter(bool &prev_input_bit, bool input_bit, bool &sample_point, bool &writing_point);
 
     public:
         BitTimingLogic();

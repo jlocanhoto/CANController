049735f9903def778ea7ee5b6be456bbdb0964c4
jlocx <jloc@cin.ufpe.br>
2018-11-27 08:30:36 -0300

    Update 'arb_wr_pt' to 'arb_wp'

--- datatypes/Bit_Stuffing_Writing_DataType.h
+++ datatypes/Bit_Stuffing_Writing_DataType.h
@@ -2,7 +2,7 @@
 #define BIT_STUFFING_WRITING_DATATYPE_H_INCLUDE
 
 typedef struct bit_stuffing_writing_output {
-    bool arb_wr_pt;
+    bool arb_wp;
     bool output_bit;
 } Bit_Stuffing_Writing_Output;
 

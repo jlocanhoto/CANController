e8af3efc371644fa1cfc685b8e1e8e9d95cb1864
Lucas Melo <lnm@cin.ufpe.br>
2018-11-30 08:34:04 -0300

    Adding error treatment

---
+++ Error.cpp
@@ -0,0 +1,60 @@
+#include "Error.h"
+
+Error::Error(Error_Data &output)
+{
+    output.error_detected = LOW;
+    this->output = &output;
+    this->state = INIT__error_treatment__;
+}
+
+void Error::connect_inputs(Bit_Stuffing_Reading_Data &Bit_Stuff_Read, Decoder_Data &decoder, Frame_Transmitter_Data &Frame_Transmitter)
+{
+    this->input.Bit_Stuff_Read = &Bit_Stuff_Read;
+    this->input.decoder = &decoder;
+    this->input.Frame_Transmitter = &Frame_Transmitter;
+}
+
+void Error::run()
+{
+    switch(this->state)
+    {
+        case INIT__error_treatment__:
+        {
+            this->output->error_detected = LOW;
+            if (this->input.Bit_Stuff_Read->stuff_error == HIGH || this->input.decoder->crc_error == HIGH || this->input.decoder->format_error == HIGH || this->input.Frame_Transmitter->bit_error == HIGH || this->input.Frame_Transmitter->ack_error == HIGH) {
+                this->state = ERROR__error_treatment__;
+            }
+            break;
+        }
+        case ERROR__error_treatment__:
+        {
+            if (this->input.Bit_Stuff_Read->stuff_error == HIGH) {
+            this->output->error_detected = HIGH;
+            this->output->error_state = 
+            this->state = INIT__error_treatment__;
+            }
+            else if (this->input.decoder->crc_error == HIGH) {
+                this->output->error_detected = HIGH;
+                this->output->error_state = 
+                this->state = INIT__error_treatment__;
+            }
+            else if (this->input.decoder->format_error == HIGH) {
+                this->output->error_detected = HIGH;
+                this->output->error_state = 
+                this->state = INIT__error_treatment__;
+            }
+            else if (this->input.Frame_Transmitter->bit_error == HIGH) {
+                this->output->error_detected = HIGH;
+                this->output->error_state = 
+                this->state = INIT__error_treatment__;
+            }
+            else if (this->input.Frame_Transmitter->ack_error == HIGH) {
+                this->output->error_detected = HIGH;
+                this->output->error_state = 
+                this->state = INIT__error_treatment__;
+            }
+            break;
+        }
+
+    }
+}
--- Error.h
+++ Error.h
@@ -5,4 +5,28 @@
 #define ACTIVE_ERROR_CODE   1
 #define PASSIVE_ERROR_CODE  2
 
+#include "datatypes/datatypes.h"
+
+typedef enum error_states {
+    INIT__error_treatment__,
+    ERROR__error_treatment__
+} Error_States;
+
+typedef struct error_treatment_input {
+    Bit_Stuffing_Reading_Data* Bit_Stuff_Read;
+    Decoder_Data* decoder;
+    Frame_Transmitter_Data* Frame_Transmitter;
+} Error_Input;
+
+class Error {
+    private:
+        Error_Input input;
+        Error_Data* output;
+        Error_States state;
+        uint8_t count;
+    public:
+        Error(Error_Data &output);
+        void connect_inputs(Bit_Stuffing_Reading_Data &Bit_Stuff_Read, Decoder_Data &decoder, Frame_Transmitter_Data &Frame_Transmitter);
+        void run();
+}
 #endif
